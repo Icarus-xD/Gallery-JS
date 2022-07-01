ddfa6a1ea52e916e324a8c7c67c8ced9a0f16ef1
Icarus_xD <semen.nine@yandex.ru>
2022-07-01 22:31:55 +0500

    Day 4

--- scripts/const.js
+++ scripts/const.js
@@ -4,6 +4,6 @@ export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize';
 export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 export const ACCESS_KEY = 'E-Wx71R1xawFLfx5wL0vYE1r4pzg6nCO1EoAu2dBMWw';
 export const SECRET_KEY = 'B05eNOhiuk49RTPKE1NleHvgU5BOKDYGDyQNTZRm7CM';
-export const REDIRECT_URI = 'https://icarus-xd.github.io/Gallery-JS/';
+export const REDIRECT_URI = 'https://gallery-js-roan.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
